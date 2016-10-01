1d008cc48247dd3aad742d8601916f389471f54e
Sagi Uziel <sagiuziel2@gmail.com>
2016-10-01 19:23:12 +0300

    sagi

--- Services/serverSrv.js
+++ Services/serverSrv.js
@@ -20,14 +20,12 @@ function errorDB(error) {
 }
 
 var db = SQLite.openDatabase({name: 'WriteNow.db', location: 'default'}, null , errorDB);
-var isFirstTime = true;
+var isFirstTime_Friends = true;
+var isFirstTime_Chats = true;
 export var myFriends = null;
+export var myChats = null;
 
-
-db.transaction((tx) => { 
-     // tx.executeSql('Drop table Friends');
-});
-
+//Users
 export function GetAllMyFriends(callback, isUpdate) {    
     try {
         if(myFriends && callback && !isUpdate){
@@ -55,8 +53,8 @@ export function GetAllMyFriends(callback, isUpdate) {
                     myFriends = result;
                     if (callback) {
                         callback(result);
-                        if(isFirstTime == true){
-                            isFirstTime = false;
+                        if(isFirstTime_Friends == true){
+                            isFirstTime_Friends = false;
                             GetAllMyFriends_Server(callback);
                         }
                     }
@@ -65,7 +63,7 @@ export function GetAllMyFriends(callback, isUpdate) {
                 }                
               }, errorDB);
         }, (error) => {
-            ErrorHandler.WriteError('serverSrv.js => transaction', error);
+            ErrorHandler.WriteError('serverSrv.js => GetAllMyFriends => transaction', error);
         });
     } catch (error) {
         ErrorHandler.WriteError('serverSrv.js => GetAllMyFriends', error);
@@ -83,7 +81,6 @@ function GetAllMyFriends_Server(callback) {
             db.transaction((tx) => { 
                 for(var i = 0; i < data.length; i++)
                 {
-                    console.log(data[i].id);
                     if (data[i].deletedUser == true && data[i].id) {
                         tx.executeSql('DELETE FROM Friends WHERE id=?', [data[i].id]);
                     } else {
@@ -98,7 +95,6 @@ function GetAllMyFriends_Server(callback) {
                             data[i].publicInfo.gender ]);
                     }
                 }
-                console.log(111);
                 GetAllMyFriends(callback, true);
             })
         }));
@@ -106,3 +102,76 @@ function GetAllMyFriends_Server(callback) {
         ErrorHandler.WriteError('serverSrv.js => GetAllMyFriends_Server', error);
     }
 } 
+
+//Conversation
+export function GetAllUserConv(callback, isUpdate) {    
+    try {
+        if(myChats && callback && !isUpdate){
+            callback(myChats);
+            return;
+        }
+        db.transaction((tx) => {    
+            tx.executeSql('CREATE TABLE IF NOT EXISTS Conversation (id PRIMARY KEY NOT NULL, isEncrypted, manager , groupName, groupPicture, isGroup)', [] , null, errorDB); //להוציא לפונקציה נפרדת
+            tx.executeSql('SELECT * FROM Conversation',[], (tx, rs) => {
+                try{
+                    var result = [];
+                    for(var i = 0; i < rs.rows.length; i++)
+                    {
+                        result.push({id: rs.rows.item(i).id,
+                                    isEncrypted: rs.rows.item(i).isEncrypted,
+                                    manager: rs.rows.item(i).manager,
+                                    groupName: rs.rows.item(i).groupName,
+                                    groupPicture: rs.rows.item(i).groupPicture,
+                                    isGroup: rs.rows.item(i).isGroup
+                        });
+                    }
+                    myChats = result;
+                    if (callback) {
+                        callback(result);
+                        if(isFirstTime_Chats == true){
+                            isFirstTime_Chats = false;
+                            GetAllMyFriends_Server(callback);
+                        }
+                    }
+                }catch(error){
+                    ErrorHandler.WriteError('serverSrv.js => SELECT * FROM Conversation => catch', error);
+                }                
+              }, errorDB);
+        }, (error) => {
+            ErrorHandler.WriteError('serverSrv.js => GetAllUserConv => transaction', error);
+        });
+    } catch (error) {
+        ErrorHandler.WriteError('serverSrv.js => GetAllUserConv', error);
+    }
+} 
+
+function GetAllUserConv_Server(callback) {    
+    try {
+        var chats = [];
+        if (myChats) {
+            chats = myChats;
+        }
+        let convIdArray = chats.map((chat) => {return chat.id;});
+        socket.emit('GetAllUserConvChanges', convIdArray, ((data) => {
+            db.transaction((tx) => { 
+                for(var i = 0; i < data.length; i++)
+                {
+                    if (data[i].deletedConv == true && data[i].id) {
+                        tx.executeSql('DELETE FROM Conversation WHERE id=?', [data[i].id]);
+                    } else {
+                        tx.executeSql('INSERT INTO Conversation VALUES (?, ?, ?, ?, ?, ?)', 
+                            [data[i].id,
+                             data[i].isEncrypted,
+                             data[i].manager,
+                             data[i].groupName,
+                             data[i].groupPicture,
+                             data[i].isGroup ]);
+                    }
+                }
+                GetAllUserConv(callback, true);
+            })
+        }));
+    } catch (error) {
+        ErrorHandler.WriteError('serverSrv.js => GetAllUserConv_Server', error);
+    }
+}  
