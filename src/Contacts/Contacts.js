c10f5c4cd603aeec046bba174a56fa0ae705c7d6
Zrahia <Zrahia@gmail.com>
2016-10-01 14:33:40 +0300

    Contacts commit

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,33 +1,28 @@
 import React, { Component } from 'react';
-import { Container, Content, List, ListItem, Thumbnail, Text } from 'native-base';
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
-});
+import { Image,
+    ReactNative,
+    ListView,
+    TouchableHighlight,
+    StyleSheet,
+    RecyclerViewBackedScrollView,
+    Text,
+    View, } from 'react-native';
 
 export default class Contacts extends Component {
+    constructor() {
+        super();
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+        };
+    }
 
-
-  render() {
-    return (     
-            <Container style={[styles.container]}>
-                <Content style={[styles.container]}>
-                    <List style={[styles.container]}>
-                        <ListItem style={[styles.container]}>
-                            <Text>Simon Mignolet</Text>
-                        </ListItem>
-                        <ListItem>
-                            <Text>Nathaniel Clyne</Text>
-                        </ListItem>
-                        <ListItem>
-                            <Text>Dejan Lovren</Text>
-                        </ListItem>
-                    </List>
-                </Content>
-            </Container>     
-    );
-  }
+    render() {
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={(rowData) => <Text>{rowData}</Text>}
+                />
+        );
+    }
 }
