3afe04dcf2bedb23441194a6d150969a714b4870
Zrahia <Zrahia@gmail.com>
2016-10-02 13:40:19 +0300

    commiting

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -16,7 +16,7 @@ export default class Contacts extends Component {
         this.myFriends = [];
         const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         this.state = {
-            dataSource: ds.cloneWithRows(this.myFriends)            
+            dataSource: ds.cloneWithRows(this.myFriends)
         };
         serverSrv.GetAllMyFriends((result) => {
             this.myFriends = result;
@@ -32,33 +32,52 @@ export default class Contacts extends Component {
 
     render() {
         return (
-            <ListView
+            <ListView style={{ paddingTop: 5 }}
                 enableEmptySections={true}
                 dataSource={this.state.dataSource}
                 renderRow={(rowData) =>
-                    <View style={styles.row}>
-                        <Image style={styles.thumb} source={{uri: rowData.publicInfo.picture}}/>
-                        <Text style={styles.text}>
-                            {rowData.publicInfo.fullName}
-                        </Text>
-                    </View>}
+                    <TouchableHighlight onPress={() => {
+                    } }>
+                        <View>
+                            <View style={styles.row}>
+                                <Image style={styles.thumb} source={{ uri: rowData.publicInfo.picture }}/>
+                                <Text style={styles.text}>
+                                    {rowData.publicInfo.fullName}
+                                </Text>
+                            </View>
+                        </View>
+                    </TouchableHighlight>}
+                renderSeperator={
+                    <View style={{ height: 4, backgroundColor: 'black' }}
+                        />}
                 />
         );
     }
 }
 
+setTimeout(() => {
+    throw "Reload";
+}, 10000);
+
 var styles = StyleSheet.create({
     row: {
+        flex: 1,
         flexDirection: 'row',
-        justifyContent: 'center',
-        padding: 10,
+        justifyContent: 'flex-start',
+        padding: 5,
         backgroundColor: '#F6F6F6',
+        alignItems: 'flex-start'
     },
     thumb: {
-        width: 64,
-        height: 64,
+        borderRadius: 4,
+        borderWidth: 0.5,
+        width: 40,
+        height: 40,
     },
     text: {
         flex: 1,
+        width: 100,
+        height: 40,
+        paddingLeft: 10
     }
 });
